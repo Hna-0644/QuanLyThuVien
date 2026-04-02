67f8b1684846d2a183cc081344ff871ee623db25
DTRUNG1401 <joyboy140105@gmail.com>
2026-04-02 19:52:10 +0700

    text

--- QuanLyThuVien/FormMuonTra.Designer.cs
+++ QuanLyThuVien/FormMuonTra.Designer.cs
@@ -35,6 +35,7 @@
             this.dtNgayTra = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.dgvMuonTra = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMuonTra)).BeginInit();
             this.SuspendLayout();
             // 
@@ -95,11 +96,19 @@
             this.dgvMuonTra.Size = new System.Drawing.Size(742, 177);
             this.dgvMuonTra.TabIndex = 6;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
             // FormMuonTra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dgvMuonTra);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dtNgayTra);
@@ -112,6 +121,7 @@
             this.Load += new System.EventHandler(this.FormMuonTra_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMuonTra)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -124,5 +134,6 @@
         private System.Windows.Forms.DateTimePicker dtNgayTra;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dgvMuonTra;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
